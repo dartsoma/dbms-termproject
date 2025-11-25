1b0b70b88a78b179795515e583de262ee21f07c9
Chad-soma Foster <dartsoma@github.com>
2025-11-24 20:59:27 -0500

    Got logins to work and redirect

--- index.php
+++ index.php
@@ -18,26 +18,58 @@
     </nav>
 </div>
 
+<?php
 
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'rentalOrg';
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+$user = $_POST['username'];
+$pass = $_POST['passkey'];
+
+
+$sql = "SELECT CustomerID FROM account WHERE UserName = '$user' AND PassKey = '$pass'";
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) > 0) {
+
+    $row = mysqli_fetch_assoc($result);
+    echo "Login successful! CustomerID: " . $row['CustomerID'];
+    header("Location: process_login.php");
+
+} else {
+
+    header("Location: index.php?error=Invalid user or pass");
+    exit();
+}
+}
+
+
+mysqli_close($conn);
+?>
 
 <div class="login">
 
 <h1>Login</h1>
 
-        <?php
-        if (isset($_GET['error'])) {
-            echo '<p style="color:red;">' . $_GET['error'] . '</p>';
-        }
-        ?>
-
-<form method="post" action="process_login.php"></form>
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 <h2>Username</h2> 
 
-<input type =  "text" id ="username" placeholder = "Enter Username...">
+<input type =  "text" id ="username" name = "username" placeholder = "Enter Username...">
 
 
 <h2>Password</h2> 
-<input type =  "text" id ="password" placeholder = "Enter Password...">
+<input type =  "text" id ="password" name = "passkey" placeholder = "Enter Password...">
 <input type = "submit" value = "Submit">
 
 </form>
--- process_login.php
+++ process_login.php
@@ -1,37 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel = "stylesheet" href = "style/home.css">
+  <title>Car Rental Company</title>
+</head>
+<body>
+<div class = "titlebar">
+    <nav>
+        <ul>
+            <li class = "left"><a>For Sale</a></li>
+            <li class = "left"><a>Maintanence</a></li>
+            <li class = "left"><a>Help</a></li>
+            <li class = "right"><a>Register</a></li>
+        </ul>
+    </nav>
+</div>
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'rentalOrg';
+<div class="login">
 
-$conn = mysqli_connect($host, $username, $password, $database);
+<h1>Login Completed</h1>
 
+</div>
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-
-$user = $_POST['username'];
-$pass = $_POST['passkey'];
-
-
-$sql = "SELECT CustomerID FROM users WHERE UserName = '$user' AND PassKey = '$pass'";
-$result = mysqli_query($conn, $sql);
-
-
-if (mysqli_num_rows($result) > 0) {
-
-    $row = mysqli_fetch_assoc($result);
-    echo "Login successful! CustomerID: " . $row['CustomerID'];
-
-} else {
-
-    header("Location: index.php?error=Invalid username or password");
-    exit();
-}
-
-
-mysqli_close($conn);
-?>
+</body>
+</html>
